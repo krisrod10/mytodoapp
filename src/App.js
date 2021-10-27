2ff82486f58a073b4928d53708a7eb3fcd512654
krisrod10 <krisrodgers12@icloud.com>
2021-10-26 22:03:46 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isClicked: false,
+      todo: [],
+      test: "",
+    };
+    //Fix the 'this' binding = HARD WAY
+    // this.handleClick = this.handleClick.bind(this);
+  }
+  // componentDidMount(){
+  //console.log("I mounted")
+  //}
+
+  componentDidUpdate() {
+    // console.log("After Update", this.state.isClicked);
+    console.log("Updated text:", this.state.text);
+  }
+
+  // this.setState({ isClicked: true, ....})
+  handleClick = () => {
+    console.log("Clicked");
+    // console.log("In function", this);
+    console.log(this.state.isClicked);
+    this.setState({
+      isClicked: !this.state.isClicked,
+    });
+    console.log("Before", this.state.isClicked);
+    // everytime you push your button it will be adding todos with your text input when clicked on the button
+  }
+
+  handleChange = (event) => {
+    console.log("event", event);
+    this.setState({
+      text: event.target.value,
+    })
+  }
+
+handleDelete = () => {
+  console.Log()
 }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>TODO App</h1>
+        <input onChange={this.handleChange} />
+        <button onClick={this.handleClick}>Click me</button>
+      </div>
+    );
+  }
+}
+
 
 export default App;
